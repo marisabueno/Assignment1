b8a860e7b086e3b16fc01add51bd619bf4438173
marisabueno <marisa.bueno@gritacademy.se>
2025-10-03 09:09:10 +0200

    Add files via upload

---
+++ classe 2/scripts.js
@@ -0,0 +1,6 @@
+alert("You can't divide us!")
+let foo = 0;
+let bar = 0;
+if (!Number.isFinite(result)){alert("You can't divide us!")}; console.warn('invalid division result', result )
+    console.log(result);
+
